f8c3d0e5f50e064c5448f2a7ed61f8a93db94a90
DarkGhosthunter <DarkGhostHunter@gmail.com>
2021-06-12 03:05:35 -0400

    Added tooltips and styling.

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import Collapse from "bootstrap/js/src/collapse"
 import Alert from "bootstrap/js/src/alert"
 import Dropdown from "bootstrap/js/src/dropdown"
 import Modal from "bootstrap/js/src/modal"
+import Tooltip from "bootstrap/js/src/tooltip"
 
 //
 // Enable Dark Mode.
@@ -10,28 +11,42 @@ import Modal from "bootstrap/js/src/modal"
 window.DarkMode = DarkMode.detect(); // Detect dark mode and save it as global variable.
 
 //
-// Activate all collapsible elements.
+// Activate all collapsable.
 //
 Array.from(document.getElementsByClassName('collapse'))
-    .forEach(element => new Collapse(element, {
+    .map(element => new Collapse(element, {
         toggle: false // Don't toggle the Collapsing elements automatically.
     }))
 
 //
-// Activate all alert elements.
+// Activate all alerts.
 //
 Array.from(document.getElementsByClassName('alert'))
-    .forEach(element => new Alert(element))
+    .map(element => new Alert(element))
 
 //
 // Activate all dropdowns.
 //
 Array.from(document.getElementsByClassName('dropdown-toggle'))
-    .forEach(element => new Dropdown(element))
+    .map(element => new Dropdown(element))
 
 //
 // Activate all modals.
 //
 Array.from(document.getElementsByClassName('modal'))
     .filter(element => element.id) // Filter those who have an ID.
-    .forEach(element => new Modal(element))
+    .map(element => new Modal(element))
+
+//
+// Activate all tooltips.
+//
+Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    .map(element => {
+        let tooltip = new Tooltip(element)
+
+        if (element.hasAttribute('data-bs-show')) {
+            tooltip.show()
+        }
+
+        return tooltip
+    })
